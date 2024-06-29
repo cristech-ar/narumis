ba2ecca836208a7bcb71cf52668df99dca2de4f8
Cristian Ríos <cjrios.info@gmail.com>
2024-06-29 05:37:35 +0000

    initial commit

---
+++ client/narumis-ui/src/App.js
@@ -0,0 +1,12 @@
+import AppAppBar from './components/AppAppBar';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <AppAppBar/>
+    </div>
+  );
+}
+
+export default App;
